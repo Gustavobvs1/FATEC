8e959efa7295908d3d3a01e84a35514b960ad92a
Gustavobvs1 <gustavobvs06@gmail.com>
2024-09-02 19:50:04 -0300

    a

---
+++ java/beecrowd/Ex1003.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex1003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+
+        System.out.println("SOMA = " + (n1 + n2));
+    }
+}
