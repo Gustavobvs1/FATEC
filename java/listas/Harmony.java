cde5de6735920821283ee12329808bb89910e7fa
Gustavo Borges <gustavobvs06@gmail.com>
2024-09-23 20:04:40 -0300

    Add files via upload

---
+++ java/listas/Harmony.java
@@ -0,0 +1,16 @@
+package java;
+
+import java.util.Scanner;
+
+public class Harmony {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        double sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += (1d/i);
+        }
+        System.out.println(sum);
+    }
+}
